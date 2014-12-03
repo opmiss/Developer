0924dc736914b3782a4504c0f98ac1889d726af6
opmiss <opmiss@gmail.com>
2014-12-03 14:20:54 -0800

    20141203

--- Day3/app.py
+++ Day3/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/', defaults={'name':"Guest"})
 @app.route('/', methods=['GET'])
 def say_hello(name):
-	return "Hello "+name
+    return "Hello "+name
 
 if __name__=="__main__":
-	app.run(debug=True)
+    app.run(debug=True)
